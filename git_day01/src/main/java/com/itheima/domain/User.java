c57663a16979deceef70c188ad5c00ea6088c2ed
wanghd0 <wanghd0@163.com>
2020-03-26 21:49:56 +0800

    增加属性

--- git_day01/src/main/java/com/itheima/domain/User.java
+++ git_day01/src/main/java/com/itheima/domain/User.java
@@ -1,7 +1,12 @@
 package com.itheima.domain;
 
+import java.util.Date;
+
 public class User {
     private Integer id;
     private String name;
     private String address;
+
+    private String sex;
+    private Date birthday;
 }
